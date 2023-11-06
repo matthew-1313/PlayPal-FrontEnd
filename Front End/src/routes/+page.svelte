2a95ea8ec7933601c3387f1d5d8b6c5b212d2df5
Danmoham <dmoham35@gmail.com>
2023-11-06 13:41:07 +0000

    done

--- Front End/src/routes/+page.svelte
+++ Front End/src/routes/+page.svelte
@@ -1,15 +1,21 @@
 <script>
   import SignUp from "./SignUp/+page.svelte";
   import SignIn from "./SignIn/+page.svelte"
-  import { Router, Route, Link } from "svelte-navigator";
-  export const informationArray = []
-  let SignState = ""
+  import { Router, Link, Route } from "svelte-routing";
+  export const name = "Hello"
+  export let url = "";
 </script>
+<Router {url}>
 <div>
 <p>Please click one of the below to continue</p>
 <a href="/SignUp"><button>Sign Up</button></a>
 <a href="/SignIn"><button>Log In</button></a>
+<div>
+  <Route path="/SignUp" component={SignUp} />
+  <Route path="/SignIn" component={SignIn} />
 </div>
+</div>
+</Router>
 
 
 
