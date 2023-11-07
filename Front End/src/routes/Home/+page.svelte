e0a83bff3c0ee0890242adb2d8bbfb172b317559
Emory <ro25ck@yahoo.com>
2023-11-07 15:19:51 +0000

    removed non-used folders from file strucutre

--- Front End/src/routes/Home/+page.svelte
+++ Front End/src/routes/Home/+page.svelte
@@ -7,4 +7,10 @@
 <Navbar />
 <h1>This is the home page</h1>
 
+<p>Insert search bar here</p>
+
+<p>user connections online status here</p>
+
+<p>show upcoming events here</p>
+
 <GamesCardHome />
--- Front End/src/routes/SingleGamePage/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  const gameId = url.searchParams.get("gameId");
-  console.log(gameId);
-</script>
-
-<h2>single game page</h2>
