a3f30f9120cb3cd79ba6483f90af5b8699d36e24
Emory <ro25ck@yahoo.com>
2023-11-08 09:29:01 +0000

    made minor changes to single game page data mapping

--- Front End/src/routes/Games/[gameId]/+page.svelte
+++ Front End/src/routes/Games/[gameId]/+page.svelte
@@ -2,6 +2,7 @@
   import { page } from "$app/stores";
   import { getGameById } from "../../../lib/api";
   import { data } from "../../../lib/store";
+  import Navbar from "../../../lib/navbar.svelte"
   let gameId = $page.params.gameId;
 
   async function fetchData(gameId) {
@@ -16,14 +17,34 @@
   }
 
 </script>
-
-<h2>{gameId}</h2>
-
+<Navbar />
 {#await fetchData(gameId)}
 <p>loading</p>
 {:then data}
-<p>{data.id}</p>
+<div>
+  <img src={data.image} alt={data.name}>
 <p>{data.name}</p>
+<p>{data.released}</p>
+{#each data.parent_platforms_arr as parent}
+| {parent.platform.name} |
+{/each}
+<p>Metacritic: {data.metacritic}</p>
+<p>{data.website}</p>
+</div>
 {:catch error}
 <p>{error.message}</p>
 {/await}
+
+<style>
+  img {
+    max-width: 70%;
+    height: auto;
+    align-items: center;
+    vertical-align: middle;
+    font-style: italic;
+    background-repeat: no-repeat;
+    background-size: cover;
+    shape-margin: 0.75rem;
+    border-radius: 0px 0px 10px 10px;
+  }
+</style>
