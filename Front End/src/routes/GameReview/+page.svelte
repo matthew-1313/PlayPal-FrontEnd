c697051f1fd3e539abad9b9abdd2a75ccf4b2beb
Matthew <matthew.d.g.mathieson@gmail.com>
2023-11-09 11:00:58 +0000

    added some sudo code for clarity

--- Front End/src/routes/GameReview/+page.svelte
+++ Front End/src/routes/GameReview/+page.svelte
@@ -3,27 +3,33 @@
   import { db } from "../../lib/firebase/firebase.client";
   import { MyUser } from "../../lib/store";
   import { get } from "svelte/store";
-  console.log(MyUser.username);
 
-  //need to send username, fetch user avatar, game id, body to be storeed on the backend
+  //gets the current gameId from the url params, and sets it as game_id to be sent off in submitReview
   export let gameId;
+  let game_id = gameId;
+
+  //sets userRating to default 0, until a rating button is pushed
+  let userRating = 0;
+
   const username = get(MyUser);
 
+  //these are the users final review/title to be sent off in submitReview
   let userReview = "";
-  let reviewTitle = "";
   let userReviewTitle = "";
+
+  //these can be ignored, they keep track of the input and textarea fields so they can be reset on submit/discard
+  let reviewTitle = "";
   let reviewField;
   let newReviewValue = "";
   let newTitleValue = "";
-  let userRating = 0;
-  let game_id = gameId;
 
+  //sets userRating based on 1-5 button clicks, to be sent off in submitReview
   function setRating(num) {
     userRating = num;
-    // console.log(userRating);
   }
 
   async function submitReview() {
+    //sends review
     const docRef = await addDoc(collection(db, "Reviews"), {
       username: username,
       user_avatar: "",
@@ -33,13 +39,14 @@
       user_game_rating: userRating,
       created_at: Timestamp.fromDate(new Date("December 10, 1815")),
     });
+    //resets data fields
     reviewField.value = "";
     userRating = 0;
     reviewTitle.value = "";
-    // console.log("Document written with ID: ", docRef.id);
   }
 
   function discardReview() {
+    //resets data fields
     reviewField.value = "";
     userRating = 0;
     reviewTitle.value = "";
@@ -100,7 +107,6 @@
     bind:value={newReviewValue}
     on:change={(event) => {
       userReview = event.target.value;
-      //console.log(userReview);
     }}
   />
   <br />
