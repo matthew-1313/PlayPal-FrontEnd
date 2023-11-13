28f71850a07aecb60c8e93eb79fd2afcf85c7bb3
swoody2527 <swoodall2527@gmail.com>
2023-11-13 14:54:39 +0000

    integrated talkJS messaging

--- Front End/src/routes/Home/+page.svelte
+++ Front End/src/routes/Home/+page.svelte
@@ -1,50 +1,47 @@
 <script>
-import GamesCardHome from "../../lib/gamesCardHome.svelte";
-import Navbar from "../../lib/navbar.svelte";
-// import { MyUser } from "../../lib/store";
-// import { data } from "../../lib/store";
-import { onMount } from "svelte";
-import Talk from 'talkjs';
-import {StoredUserInfo} from "../../lib/store"
-// let user;
+  import GamesCardHome from "../../lib/gamesCardHome.svelte";
+  import Navbar from "../../lib/navbar.svelte";
+  // import { MyUser } from "../../lib/store";
+  // import { data } from "../../lib/store";
+  import { onMount } from "svelte";
+  import Talk from "talkjs";
+  import { StoredUserInfo } from "../../lib/store";
+  // let user;
 
-// MyUser.subscribe((value) => {
-//   user = value;
-// });
+  // MyUser.subscribe((value) => {
+  //   user = value;
+  // });
 
-onMount(async () => {
-  await Talk.ready;
-});
+  onMount(async () => {
+    await Talk.ready;
 
-const currentUser = new Talk.User({
-  id: $StoredUserInfo.username,
-  name: $StoredUserInfo.username,
-  photoUrl: $StoredUserInfo.avatar_url,
-});
+    const currentUser = new Talk.User({
+      id: $StoredUserInfo.username,
+      name: $StoredUserInfo.username,
+      photoUrl: $StoredUserInfo.avatar_url,
+    });
 
-const otherUser = new Talk.User({
-  id: 'Jessie',
-  name: 'Jessie',
-  photoUrl: 'https://img.freepik.com/premium-vector/man-headphones-mascot-logo-gaming-vector-illustration_382438-453.jpg',
-});
+    const otherUser = new Talk.User({
+      id: "Mike",
+      name: "Mike",
+      photoUrl:
+        "https://img.freepik.com/premium-vector/man-headphones-mascot-logo-gaming-vector-illustration_382438-453.jpg",
+    });
 
-const session = new Talk.Session({
-    appId: 'tkzuhGNe',
-    me: currentUser,
-});
+    const session = new Talk.Session({
+      appId: "tkzuhGNe",
+      me: currentUser,
+    });
+    const conversationID = Talk.oneOnOneId(currentUser, otherUser);
+    const conversation = session.getOrCreateConversation(conversationID);
+    conversation.setParticipant(currentUser);
+    conversation.setParticipant(otherUser);
+    const popup = session.createPopup()
+    popup.select(conversation)
+    popup.mount({show: false})
 
-const conversationID = Talk.oneOnOneId(currentUser, otherUser);
-const conversation = session.getOrCreateConversation(conversationID);
-conversation.setParticipant(currentUser);
-conversation.setParticipant(otherUser);
-
-const inbox = session.createInbox();
-inbox.select(conversation)
-
-let inboxEl
-
-inbox.mount(this.inboxEl)
-
+  
+  });
 </script>
 
 <Navbar />
@@ -57,4 +54,3 @@ inbox.mount(this.inboxEl)
 <p>show upcoming events here</p>
 
 <GamesCardHome />
-<div bind:this={inboxEl} />
